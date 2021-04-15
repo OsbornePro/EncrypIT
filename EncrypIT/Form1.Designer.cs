f915c0892f3adbc87d41f7156636e4c10835d37f
tobor <rosborne@osbornepro.com>
2021-04-15 12:50:03 -0600

    Initial Commit
    Initial Commit

---
+++ EncrypIT/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace EncrypIT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(8, 173);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(377, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Encrypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.AllowDrop = true;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox1.Location = new System.Drawing.Point(8, 85);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(772, 82);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Drag and Drop file here";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            this.textBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.TextBox1_DragDrop);
+            this.textBox1.DragOver += new System.Windows.Forms.DragEventHandler(this.TextBox1_DragOver);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(8, 12);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(772, 67);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "Drag the file or folder you want to encrypt or decrypt into the box below. \r\nThen" +
+    " click the a button to encrypt or decrypt the file.";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(12, 231);
+            this.label1.MaximumSize = new System.Drawing.Size(772, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Status Information";
+            this.label1.Click += new System.EventHandler(this.TextBox2_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(402, 174);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(378, 44);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Decrypt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(792, 314);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "EncrypIT (Encrypt your files using EFS)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ EncrypIT/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace EncrypIT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void TextBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.All;
+            else
+
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void TextBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = e.Data.GetData(DataFormats.FileDrop) as string[]; // get all files droppeds  
+            if (files != null && files.Any())
+                textBox1.Text = files.First(); //select the first one  
+        }
+
+        private void TextBox1_DragOver(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Link;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string strValue = textBox1.Text;
+
+            if (File.Exists(strValue))
+            {
+                label1.Text = $"{strValue} file location verified. Encrypting file...";
+                File.Encrypt(textBox1.Text);
+            }  // End If
+            else if (!File.Exists(strValue))
+            {
+                label1.Text = $"{strValue} file does NOT exist";
+
+                if (Directory.Exists(strValue))
+                {
+                    label1.Text = $"{strValue} directory location verified. Encrypting folder...";
+                    File.Encrypt(textBox1.Text);
+                }  // End If
+                else if (!Directory.Exists(strValue))
+                {
+                    label1.Text = $"{strValue} directory does NOT exist.";
+                }  // End Else
+
+            }  // End Else If
+            label1.Text = $"Completed encryption of {strValue}";
+        }
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            string strValue = textBox1.Text;
+
+            if (File.Exists(strValue))
+            {
+                label1.Text = $"{strValue} file location verified. Decrypting file...";
+                File.Decrypt(strValue);
+            }  // End If
+            else if (!File.Exists(strValue))
+            {
+                label1.Text = $"{strValue} file does NOT exist";
+
+                if (Directory.Exists(strValue))
+                {
+                    label1.Text = $"{strValue} directory location verified. Decrypting folder...";
+                    File.Decrypt(strValue);
+                }  // End If
+                else if (!Directory.Exists(strValue))
+                {
+                    label1.Text = $"{strValue} directory does NOT exist.";
+                }  // End Else
+
+            }  // End Else If
+            label1.Text = $"Completed decryption of {strValue}";
+        }
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
